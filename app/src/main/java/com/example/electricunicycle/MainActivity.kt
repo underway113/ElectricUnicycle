3a765f8c51f2c0d768112c339aa5407e92e7c3f5
underway <underway113@gmail.com>
2021-10-21 00:09:32 +0700

    add font + adjust UI card

--- app/src/main/java/com/example/electricunicycle/MainActivity.kt
+++ app/src/main/java/com/example/electricunicycle/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.electricunicycle.adapter.EucCardAdapter
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rvMain = findViewById<RecyclerView>(R.id.rvMain)
-//        tvTest.text = "Henlo From Indonesia"
+        rvMain.adapter = EucCardAdapter(applicationContext)
+        rvMain.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/com/example/electricunicycle/adapter/EucCardAdapter.kt
@@ -0,0 +1,72 @@
+package com.example.electricunicycle.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.electricunicycle.R
+import com.example.electricunicycle.data.DataSource
+import com.example.electricunicycle.model.EucTypeEnum
+import java.text.NumberFormat
+
+class EucCardAdapter(
+        private val context: Context
+): RecyclerView.Adapter<EucCardAdapter.EucCardViewHolder>() {
+
+    private val data = DataSource.eucList
+
+    class EucCardViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val ivItem: ImageView = view.findViewById(R.id.ivItem)
+        val tvName: TextView = view.findViewById(R.id.tvName)
+        val tvPrice: TextView = view.findViewById(R.id.tvPrice)
+        val tvInstallmentAvailable: TextView = view.findViewById(R.id.tvInstallmentAvailable)
+        val tvType: TextView = view.findViewById(R.id.tvType)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EucCardViewHolder {
+        var adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return EucCardViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: EucCardViewHolder, position: Int) {
+
+        val item = data[position]
+        val convertPrice =  NumberFormat.getInstance().format(item.price);
+
+        holder.ivItem.setImageResource(item.image)
+        holder.tvName.text = item.name
+        holder.tvPrice.text = context.resources?.getString(R.string.price_string, convertPrice.toString())
+        if (item.installmentOption) {
+            holder.tvInstallmentAvailable.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_toll_24, 0, 0, 0);
+            holder.tvInstallmentAvailable.text = context.resources?.getString(R.string.installment_available)
+        }
+        else {
+            holder.tvInstallmentAvailable.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_account_balance_wallet_24, 0, 0, 0);
+            holder.tvInstallmentAvailable.text = context.resources?.getString(R.string.installment_unavailable)
+        }
+        when(item.type) {
+            EucTypeEnum.ENTRY -> {
+                holder.tvType.setBackgroundResource(R.drawable.type_background_entry)
+                holder.tvType.text = context.resources?.getString(R.string.entry_level)
+            }
+            EucTypeEnum.HIGH -> {
+                holder.tvType.setBackgroundResource(R.drawable.type_background)
+                holder.tvType.text = context.resources?.getString(R.string.high_level)
+            }
+        }
+        holder.tvType.setPadding(dpAsPixels(8), dpAsPixels(1), dpAsPixels(8), dpAsPixels(1))
+    }
+
+    private fun dpAsPixels(sizeInDp: Int): Int {
+        val scale = context.resources?.displayMetrics?.density
+        if (scale != null) {
+            return  (sizeInDp * scale + 0.5f).toInt()
+        }
+        return sizeInDp
+    }
+}
